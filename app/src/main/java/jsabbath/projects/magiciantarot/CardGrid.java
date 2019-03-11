17e90e461093723e39577a152eb9ead8fe3bc63b
jsabbath <jsabbath@asu.edu>
2019-03-11 01:21:15 -0700

    Added menu screen button layout and background image

---
+++ app/src/main/java/jsabbath/projects/magiciantarot/CardGrid.java
@@ -0,0 +1,13 @@
+package jsabbath.projects.magiciantarot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardGrid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_grid);
+    }
+}
